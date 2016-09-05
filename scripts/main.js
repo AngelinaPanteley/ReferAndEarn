a6bf064ecf7eba151f0a905da7cf733fc29b5072
AngelinaPanteley <panteley-00@mail.ru>
2016-09-05 13:10:50 +0300

    product-page, tabs-popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 var main = function(){
+  $('.flexslider').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails"
+  });
 
+  $(".refer-and-earn-btn").on("click", function(e){
+    $(".tabs-popup").addClass("active-popup");
+    $(".product-page").append('<div class="fade"></div>');
+  });
+
+  $(".tabs-popup-cls").on("click", function(e){
+    $(".tabs-popup").removeClass("active-popup");
+    $(".fade").remove();
+  });
+
+  $(".tab-container").tabs();
+
+  $('.tab').on('click', function(){
+  	$('.tab').removeClass('active-tab');
+  	$(this).addClass('active-tab');
+  });
 };
 
 $(document).ready(main);
-
