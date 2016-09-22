6983c1baa8cda9b49abb2314e378f928c195dae7
AngelinaPanteley <panteley-00@mail.ru>
2016-09-22 11:07:22 +0300

    account company + my earnings

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 var main = function(){
-  $('.flexslider').flexslider({
-    animation: "slide",
-    controlNav: "thumbnails"
-  });
+
+  if($('.flexslider').lenght!=0){
+    $('.flexslider').flexslider({
+      animation: "slide",
+      controlNav: "thumbnails"
+    });
+  }
 
   $(".refer-and-earn-btn").on("click", function(e){
     $(".tabs-popup").addClass("active-popup");
@@ -20,6 +23,22 @@ var main = function(){
   	$('.tab').removeClass('active-tab');
   	$(this).addClass('active-tab');
   });
+
+
+  /*registration page*/
+  /*$('.registration-form__radio-span').on('click', function(){
+
+  if($('.registration-form__radio__individual').attr("checked")=="checked"){
+    $('.individual-creation').addClass('block');
+    $('.company-creation').removeClass('block');
+
+  };
+
+  if($('.registration-form__radio__company').attr("checked")=="checked"){
+    $('.individual-creation').removeClass('block');
+    $('.company-creation').addClass('block');
+  };
+  });*/
 };
 
 $(document).ready(main);
